f1b8a71b9ede2765f5d4deee2b33780f892aec20
Luiz Carlos Rocha Correa <luizcorrea@MacBook-Pro-de-Luiz.local>
2022-04-06 13:39:25 -0300

    teste

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ['pt', 'en'],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: 'pt'
+  },
+  images: {
+    loader: 'akamai',
+    path: '',
+  }
 }
 
 module.exports = nextConfig
